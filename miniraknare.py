11a75ff6f66b16ff6613bd419d6ea7a5803f821f
frej <frejlars.svedenmark@skola.taby.se>
2026-05-06 13:12:51 +0200

    Jobbade på Grafritade

--- miniraknare.py
+++ miniraknare.py
@@ -1,9 +1,7 @@
 import math as M
-import matplotlib as graf
+import matplotlib.pyplot as graf
 import re
-
-
-
+import numpy as np
 
 def addera (t1,t2):
     return (t1+t2)
@@ -16,28 +14,48 @@ def multiplisera (t1,t2):
 
 def dividera (t1,t2):
     return (t1/t2)
-    
 
+def to_the_power_of_(t1,t2):
+    return (t1**t2)
+    
 
 def equTolk(ecuation):
     ecuationlist = (re.split(" ",ecuation))
-    #for i in range (len(ecuationlist)):
+    try:
+        tal1 = (int(ecuationlist[0]))
+        opperator = ecuationlist[1]
+        tal2 = (int(ecuationlist[2]))
+        return tal1, tal2, opperator
+    except: 
+        print("Du skrev fel")
+        return 0, "+", 0
 
-    tal1 = (int(ecuationlist[0]))
-    opperator = ecuationlist[1]
-    tal2 = (int(ecuationlist[2]))
+def solve(equ,i):
+    #IIIIIIIIIIIINNNNNNNNNNNNNNNNNNNNNTTTTTTTTTTTTTTTTTTEEEEEEEEEEEEEEEEEEE klar
+    
+    pass
 
-    return tal1, tal2, opperator
+
+def Desmos2_0():
+    y = []    
+    equ = input("Ekvation med syntax, x + c")
+    if equ == 1:
+        for i in range(10):
+            y.append(to_the_power_of_(equ,i))
+    
+    graf.plot(y)
+    graf.show()
+    pass
+# IIIIIIIIIIIIIIIIIIIIIINNNNNNNNNNNNNNNTEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE klar
 
 
-# Tal opperator Tal
+
 run = True
 while run:
-
     choice = input("Grafritare(0) eller Räknare(1)")
 
-    # if choice == "0":
-    #     pass
+    if choice == "0":
+        Desmos2_0()     
 
 
     if choice == "1":
@@ -48,16 +66,12 @@ while run:
         cs = True
         if op == "+":
             a = addera(t1,t2)
-
         elif op == "-":
             a = subtrahera(t1,t2)
-        
         elif op == "*":
             a = multiplisera(t1,t2)
-        
         elif op == "/":
             a = dividera(t1,t2)
-        
         else:
             cs = False
             a = "ajabaja"
@@ -68,6 +82,4 @@ while run:
     else:
         #Felhantering
         pass
-    
-    
     
